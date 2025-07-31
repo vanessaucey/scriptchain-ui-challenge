36fe7e3e3f48e4bb88f0f1521b5529a55c4c15ec
vanessaucey <vanessa.vega@sjsu.edu>
2025-07-30 18:23:31 -0700

    Patient Card
    Displayed patient card with proper information of patient needed

--- patient-dashboard/src/app/patient-card/patient-card.ts
+++ patient-dashboard/src/app/patient-card/patient-card.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-patient-card',
+  standalone: true,
   imports: [],
   templateUrl: './patient-card.html',
   styleUrl: './patient-card.scss'
 })
 export class PatientCard {
-
+  @Input() name!: string;
+  @Input() nextAppointment!: string;
+  @Input() dob!: string;
+  @Input() sex!: string;
+  @Input() residence!: string;
+  @Input() mrn!: string;
+  @Input() id!: string;
+  @Input() ssn!: string;
+  @Input() phone!: string;
+  @Input() email!: string;
+  @Input() hospital!: string;
+  @Input() dept!: string;
+  @Input() physician!: string;
+  @Input() conditions!: string;
 }
